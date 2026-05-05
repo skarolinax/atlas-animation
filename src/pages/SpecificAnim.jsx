54b05cecf39c7202f1aac199dbe8250391d4d6c5
Karolina <k.sech@student.fontys.nl>
2026-05-05 15:13:44 +0200

    Setting up the project structure

---
+++ src/pages/SpecificAnim.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function SpecificAnim() {
+
+  return (
+    <>
+      <h1>Specific Animation</h1>
+    </>
+  )
+}
+
+export default SpecificAnim
---
+++ src/pages/UploadAnim.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function UploadAnim() {
+
+  return (
+    <>
+      <h1>Upload Animation</h1>
+    </>
+  )
+}
+
+export default UploadAnim
