4ea672f2991397caee43309d1710eb828e2b9f53
gitgitgitbrandon <526204@student.fontys.nl>
2026-05-19 11:58:32 +0200

    Add initial implementation of GrowingLetter component and related files
    - Create growingletter.jsx component with basic structure
    - Add growingletter.html file for rendering
    - Update package.json and package-lock.json to include new dependencies
    - Add styles for GrowingLetter component in growingletter.module.scss
    - Create empty CSS map file for growingletter.module.scss

---
+++ src/pages/growingletter.jsx
@@ -0,0 +1,7 @@
+import { useState, useEffect, useRef } from 'react'
+import styles from '../styles/growingletter.module.scss';
+
+function GrowingLetter() {
+    return <div className={styles.container}>...</div>;
+}
+
